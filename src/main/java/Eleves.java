a1591733dd443d013e1ba64d92dc62951a46c940
ayman072 <ayman072@hotmail.fr>
2020-07-30 13:35:24 +0200

    eleves

--- src/main/java/Eleves.java
+++ src/main/java/Eleves.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class Eleves {
+import java.sql.Date;
+
+public class Eleves {
+
+    private int idEleve;
+    private String nomEleve;
+    private String prenomEleve;
+    private Date dateNaissance;
+    private int idClasse;
+
+    public Eleves(String nomEleve, String prenomEleve, Date dateNaissance, int idClasse) {
+        this.nomEleve = nomEleve;
+        this.prenomEleve = prenomEleve;
+        this.dateNaissance = dateNaissance;
+        this.idClasse = idClasse;
+    }
+
+    public Eleves(String nomEleve, String prenomEleve, Date dateNaissance) {
+        this.nomEleve = nomEleve;
+        this.prenomEleve = prenomEleve;
+        this.dateNaissance = dateNaissance;
+    }
+
+    @Override
+    public String toString() {
+        return "Eleves{" +
+                "idEleve=" + idEleve +
+                ", nomEleve='" + nomEleve + '\'' +
+                ", prenomEleve='" + prenomEleve + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", idClasse=" + idClasse +
+                '}';
+    }
+
+    public int getIdEleve() {
+        return idEleve;
+    }
+
+    public void setIdEleve(int idEleve) {
+        this.idEleve = idEleve;
+    }
+
+    public String getNomEleve() {
+        return nomEleve;
+    }
+
+    public void setNomEleve(String nomEleve) {
+        this.nomEleve = nomEleve;
+    }
+
+    public String getPrenomEleve() {
+        return prenomEleve;
+    }
+
+    public void setPrenomEleve(String prenomEleve) {
+        this.prenomEleve = prenomEleve;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public int getIdClasse() {
+        return idClasse;
+    }
+
+    public void setIdClasse(int idClasse) {
+        this.idClasse = idClasse;
+    }
 }
